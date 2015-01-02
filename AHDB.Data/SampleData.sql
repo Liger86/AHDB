423908d870f48f505a72ad424c759efec3a146dc
Denys Olleik <dolleik@gmail.com>
2015-01-02 10:54:38 -0500

    Sample script to seed with sample data.

---
+++ AHDB.Data/SampleData.sql
@@ -0,0 +1,21 @@
+﻿insert into Customers (CompanyName) values ('Some Company 1')
+insert into Customers (CompanyName) values ('Some Company 2')
+insert into Customers (CompanyName) values ('Some Company 3')
+insert into Customers (CompanyName) values ('Some Company 4')
+insert into Customers (CompanyName) values ('Some Company 5')
+
+insert into [Services] ([Description], CustomerId) values ('Some description 1', 1)
+insert into [Services] ([Description], CustomerId) values ('Some description 2', 1)
+insert into [Services] ([Description], CustomerId) values ('Some description 3', 1)
+insert into [Services] ([Description], CustomerId) values ('Some description 4', 1)
+
+insert into [Services] ([Description], CustomerId) values ('Some description 5', 2)
+insert into [Services] ([Description], CustomerId) values ('Some description 6', 2)
+insert into [Services] ([Description], CustomerId) values ('Some description 7', 2)
+insert into [Services] ([Description], CustomerId) values ('Some description 8', 2)
+
+select * from Customers
+select * from [Services]
+
+select * from [Services]
+where CustomerId = 1
