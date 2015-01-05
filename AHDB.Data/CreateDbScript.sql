2b6344d3f099218b68df0c879014e6434367114f
Denys Olleik <dolleik@gmail.com>
2015-01-05 01:14:34 -0500

    Entity framework, packages, and config files delete.

---
+++ AHDB.Data/CreateDbScript.sql
@@ -0,0 +1,164 @@
+--For creating database
+Create database [AHDB]
+Use [AHDB]
+Go
+
+drop database [AHDB]
+GO
+
+--First create all tables
+CREATE TABLE Repair
+(
+	ID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Description] NVARCHAR(MAX) NULL,
+
+	PurchaseOrder NVARCHAR(50) NULL,
+	Completed BIT NULL, 
+
+	DateCreatedAsUtcTime DATETIME2 NOT NULL,
+	DateCompleted DATETIME2 NULL,
+	DueDate DATE NULL,
+
+	CustomerID INT NOT NULL,
+)
+GO
+
+CREATE TABLE Customer
+(
+	ID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Description] NVARCHAR(MAX),
+
+	CompanyName NVARCHAR(50) NULL,
+
+	DateCreatedAsUtcTime DATETIME2 NOT NULL,
+)
+GO
+
+CREATE TABLE Vendor
+(
+	ID int NOT NULL PRIMARY KEY IDENTITY,
+	[Description] NVARCHAR(MAX) NULL,
+
+	CompanyName NVARCHAR(50) NULL,
+
+	DateCreatedAsUtcTime DATETIME2 NOT NULL,
+)
+GO
+
+CREATE TABLE Contact
+(
+	ID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Description] NVARCHAR(MAX) NULL,
+
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NULL,
+	PhoneNumber nvarchar(50) NULL,
+	CellPhoneNumber nvarchar(50) NULL,
+	Email nvarchar(50) NULL,
+
+	DateCreatedAsUtcTime DATETIME2 NOT NULL,
+
+	CustomerID INT NULL,
+	VendorID INT NULL,
+)
+GO
+
+CREATE TABLE Note
+(
+	ID INT NOT NULL PRIMARY KEY IDENTITY,
+	NoteText NVARCHAR(max) NULL,
+
+	DateCreatedAsUtcTime DATETIME2 NOT NULL,
+
+	RepairID int NOT NULL,
+)
+GO
+
+CREATE TABLE [User]
+(
+	ID INT NOT NULL PRIMARY KEY IDENTITY,
+	[Description] NVARCHAR(MAX) NULL,
+
+	Username NVARCHAR(50) NOT NULL,
+	FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NULL,
+	[Hash] NVARCHAR(MAX) NOT NULL,
+	Salt NVARCHAR(MAX) NOT NULL,
+)
+GO
+
+--Creating relationships
+ALTER TABLE Repair add constraint Repair_CustomerID_FK
+FOREIGN KEY (CustomerID) REFERENCES Customer(ID)
+GO
+
+ALTER TABLE Contact add constraint Contact_CustomerID_FK
+FOREIGN KEY (CustomerID) references Customer(ID)
+GO
+
+ALTER TABLE Contact add constraint Contact_VendorID_FK
+FOREIGN KEY (VendorID) references Vendor(ID)
+GO
+
+ALTER TABLE Note add constraint Note_RepairID_FK
+FOREIGN KEY (RepairID) references Repair(ID)
+GO
+
+--Bridge table for many-to-many
+CREATE TABLE VendorRepair
+(
+	RepairID INT NOT NULL FOREIGN KEY REFERENCES Repair (ID),
+	VendorID INT NOT NULL FOREIGN KEY REFERENCES Vendor (ID) PRIMARY KEY(RepairID, VendorID)
+)
+GO
+
+--Stored Procedures for inserting entities
+CREATE PROC spInsertCustomer
+@Description NVARCHAR(MAX),
+@CompanyName NVARCHAR(50)
+AS
+BEGIN
+	INSERT INTO Customer ([Description], CompanyName, DateCreatedAsUtcTime) 
+	VALUES (@Description, @CompanyName, GETUTCDATE())
+END
+GO
+
+CREATE PROC spInsertRepair
+@Description NVARCHAR(MAX),
+@PurchaseOrder NVARCHAR(MAX),
+@Completed BIT,
+@DateCompleted DATETIME2,
+@DueDate DATE,
+@CustomerID INT
+AS
+BEGIN
+	INSERT INTO Repair ([Description], PurchaseOrder, Completed, DateCreatedAsUtcTime, DateCompleted, DueDate, CustomerID)
+	VALUES (@Description, @PurchaseOrder, @Completed, GETUTCDATE(), @DateCompleted, @DueDate, @CustomerID)
+END
+GO
+
+CREATE PROC spInsertVendor
+@Description NVARCHAR(MAX),
+@CompanyName NVARCHAR(50)
+AS
+BEGIN
+	INSERT INTO Vendor ([Description], CompanyName, DateCreatedAsUtcTime) 
+	VALUES (@Description, @CompanyName, GETUTCDATE())
+END
+GO
+
+CREATE PROC spInsertContact
+@Description NVARCHAR(MAX),
+@FirstName NVARCHAR(50),
+@LastName NVARCHAR(50),
+@PhoneNumber nvarchar(50),
+@CellPhoneNumber nvarchar(50),
+@Email nvarchar(50),
+@CustomerID INT,
+@VendorID INT
+AS
+BEGIN
+	INSERT INTO Contact ([Description], FirstName, LastName, PhoneNumber, CellPhoneNumber, Email, DateCreatedAsUtcTime, VendorID, CustomerID)
+	VALUES (@Description, @FirstName, @LastName, @PhoneNumber, @CellPhoneNumber, @Email, GETUTCDATE(), @VendorID, @CustomerID)
+END
+GO
---
+++ AHDB.Data/InsertSampleData.sql
@@ -0,0 +1,40 @@
+use [AHDB]
+go
+
+insert into Customer ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 1', 'Company 1', GETUTCDATE())
+insert into Customer ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 2', 'Company 2', GETUTCDATE())
+insert into Customer ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 3', 'Company 3', GETUTCDATE())
+insert into Customer ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 4', 'Company 4', GETUTCDATE())
+insert into Customer ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 5', 'Company 5', GETUTCDATE())
+insert into Customer ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 6', 'Company 6', GETUTCDATE())
+
+insert into Vendor ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 7', 'Company 7', GETUTCDATE())
+insert into Vendor ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 8', 'Company 8', GETUTCDATE())
+insert into Vendor ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 9', 'Company 9', GETUTCDATE())
+insert into Vendor ([Description], CompanyName, DateCreatedAsUtcTime) values ('Some Description 10', 'Company 10', GETUTCDATE())
+
+insert into Repair ([Description], PurchaseOrder, Completed, DateCreatedAsUtcTime, CustomerID) values ('Some Description 11', 'PO#101', 0, GETUTCDATE(), 1)
+insert into Repair ([Description], PurchaseOrder, Completed, DateCreatedAsUtcTime, CustomerID) values ('Some Description 12', 'PO#102', 0, GETUTCDATE(), 1)
+insert into Repair ([Description], PurchaseOrder, Completed, DateCreatedAsUtcTime, CustomerID) values ('Some Description 13', 'PO#103', 0, GETUTCDATE(), 2)
+insert into Repair ([Description], PurchaseOrder, Completed, DateCreatedAsUtcTime, CustomerID) values ('Some Description 14', 'PO#104', 0, GETUTCDATE(), 2)
+
+
+SELECT * FROM Customer
+SELECT * FROM Repair
+SELECT * FROM Vendor
+SELECT * FROM VendorRepair
+
+delete from VendorRepair
+
+Select * from Repair
+inner join VendorRepair ON VendorRepair.RepairID = VendorRepair.RepairID
+inner join Vendor ON VendorRepair.RepairID = Vendor.ID
+where Vendor.ID = 1
+
+Select * from Repair
+inner join VendorRepair ON VendorRepair.RepairID = VendorRepair.RepairID
+
+insert into VendorRepair (RepairID, VendorID) values (1, 2)
+insert into VendorRepair (RepairID, VendorID) values (2, 2)
+insert into VendorRepair (RepairID, VendorID) values (3, 3)
+insert into VendorRepair (RepairID, VendorID) values (4, 3)
