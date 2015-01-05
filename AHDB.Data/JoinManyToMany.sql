28163c42b6d7cc97aebaa829ac071e75830734ae
Denys Olleik <dolleik@gmail.com>
2015-01-05 01:52:39 -0500

    This is me learning how to join many to many with bridge table.

---
+++ AHDB.Data/JoinManyToMany.sql
@@ -0,0 +1,19 @@
+SELECT 
+	Repair.ID RepairID,
+	Repair.PurchaseOrder PurchaseOrder,
+	Vendor.ID VendorID,
+	Vendor.CompanyName VendorCompanyName
+		FROM Repair REPAIR
+			INNER JOIN VendorRepair VR ON REPAIR.ID = VR.RepairID
+				INNER JOIN Vendor VENDOR ON VR.VendorID = VENDOR.ID
+GO
+
+SELECT *
+	FROM Repair REPAIR
+		INNER JOIN VendorRepair VR ON REPAIR.ID = VR.RepairID
+			INNER JOIN Vendor VENDOR ON VR.VendorID = VENDOR.ID
+GO
+
+select * from VendorRepair
+GO
+
