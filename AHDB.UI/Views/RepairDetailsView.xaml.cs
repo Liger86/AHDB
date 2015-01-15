edef58f4d22ed72554cac7e688178456b26aec5d
Denys Olleik <dolleik@gmail.com>
2015-01-15 05:30:31 -0500

    Started working on repair details.

---
+++ AHDB.UI/Views/RepairDetailsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AHDB.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for RepairDetailsView.xaml
+    /// </summary>
+    public partial class RepairDetailsView : UserControl
+    {
+        public RepairDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
