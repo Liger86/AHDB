92b100b01ca0544dbdcf282e44bfcdfeb71a5b52
Denys Olleik <dolleik@gmail.com>
2015-01-21 21:13:58 -0500

    starting to implement updateRepair.

--- AHDB.Data/EntityManagers/IRepairManager.cs
+++ AHDB.Data/EntityManagers/IRepairManager.cs
@@ -9,8 +9,8 @@ namespace AHDB.Data.EntityManagers
 {
     public interface IRepairManager
     {
-        void CreateNewRepair(string description,
-                string purchaseOrder, string quoteNumber, Nullable<DateTime> dueDate, int customerID);
+        void CreateNewRepair(string description, string purchaseOrder, string quoteNumber, Nullable<DateTime> dueDate, int customerID);
+        void UpdateRepair(string description, string purchaseOrder, string quoteNumber, Nullable<DateTime> dueDate, int customerID);
         void DeleteRepair(int repairID);
         List<RepairDTO> GetAllNotCompletedRepairsAndTheirVendors();
     }
